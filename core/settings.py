af279f4b2160e4e6cfd75fcbc334ae1c98e75605
YURI MIRANDA DE MENEZES <YURI.MENEZES@rondonopolis.local>
2025-06-12 15:48:42 -0400

    hhhhhh

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,9 @@ import os
 import logging
 from dotenv import load_dotenv
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
